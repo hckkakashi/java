16e8dd316c6dfc13d13533764ae65dda63c2559e
hckkakashi <121595947+hckkakashi@users.noreply.github.com>
2023-01-24 17:46:13 +0545

    Add files via upload

---
+++ calander.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class calander {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your Name:");
+        String name=sc.nextLine();
+        System.out.println("welcome"+name);
+        System.out.println("Enter Number Between 1-12");
+        int num=sc.nextInt();
+        if (num ==1){
+            System.out.println("Its January");}
+            if (num ==2){
+                System.out.println("Its February");}
+                if (num ==3){
+                    System.out.println("Its March");}
+                    if (num ==4){
+                        System.out.println("Its April");}
+                        if (num ==5){
+                            System.out.println("Its May");}
+                            if (num ==6){
+                                System.out.println("Its June");}
+                                if (num ==7){
+                                    System.out.println("Its July");}
+                                    if (num ==8){
+                                        System.out.println("Its August");}
+                                        if (num ==9){
+                                            System.out.println("Its September");}
+                                            if (num ==10){
+                                                System.out.println("Its Octuber");}
+                                                if (num ==11){
+                                                    System.out.println("Its November");}
+                                                    if (num ==12){
+                                                        System.out.println("Its December");}
+                                                        
+                                    
+                                                                                                                                            
+    
+
+    }
+
+    
+}
